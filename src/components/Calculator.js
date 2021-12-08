56fe94f45ebff4d4e3a3664cf666db84d4e48867
Jasem Duncan <jasemvalencia@gmail.com>
2021-12-08 12:02:27 -0500

    Add new calculator.css and calculator.js files

---
+++ src/components/Calculator.js
@@ -0,0 +1,32 @@
+import './Calculator.css';
+
+const Calculator = () => (
+  <div className="App">
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="current-operand">0</div>
+      </div>
+      <button type="button">AC</button>
+      <button type="button">+/-</button>
+      <button type="button">%</button>
+      <button type="button" className="orange-buttton">/</button>
+      <button type="button">7</button>
+      <button type="button">8</button>
+      <button type="button">9</button>
+      <button type="button" className="orange-buttton">x</button>
+      <button type="button">4</button>
+      <button type="button">5</button>
+      <button type="button">6</button>
+      <button type="button" className="orange-buttton">-</button>
+      <button type="button">1</button>
+      <button type="button">2</button>
+      <button type="button">3</button>
+      <button type="button" className="orange-buttton">+</button>
+      <button type="button" className="span-two">0</button>
+      <button type="button">.</button>
+      <button type="button" className="orange-buttton">=</button>
+    </div>
+  </div>
+);
+
+export default Calculator;
